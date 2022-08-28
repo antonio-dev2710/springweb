fba605042d1e3694eaa6becf3478aed5f41c67a0
Antonio Alves <antoniocunha9610@gmail.com>
2022-08-28 20:02:31 -0300

    motando a estrutura da aplicação web

---
+++ src/main/java/com/appweb/springweb/controllers/HomeController.java
@@ -0,0 +1,17 @@
+package com.appweb.springweb.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String index(Model model){
+        //paramentro model pode enviar dados pra o template
+        model.addAttribute("nome", "Antonio");
+    return "home/index" ;   
+    }
+}
+    
+
